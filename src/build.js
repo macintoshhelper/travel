89d6aebb1f041b8afb84d44d9d371dafe85080cb
macintoshhelper <macintoshhelper@users.noreply.github.com>
2019-11-25 00:25:41 +0000

    [WIP] update data schema

--- src/build.js
+++ src/build.js
@@ -1,24 +1,49 @@
 const glob = require('glob-promise');
 const path = require('path');
+const { promisify } = require('util');
+const { readFile, existsSync } = require('fs');
+
+const readFileAsync = promisify(readFile);
 
 const dataPath = path.resolve(__dirname, '../data');
 
 const countries = require(path.join(dataPath, 'countries.json'));
 
-glob('./data/**/*.json')
-  .then(contents => {
-    console.log({ contents });
+const build = async () => {
+  for (const code in countries) {
+    const value = countries[code];
+    const { id } = value;
 
-    contents.forEach((relativePath) => {
-      const path = relativePath.replace('./data/', '');
+    const countryPath = path.join(dataPath, id);
+    const exists = existsSync(countryPath);
 
-      const foundCountries = countries.filter((country) => {
-        const { id } = country;
-        // if ( )
-      })
+    if (exists) {
+      const subdivisions = require(path.join(countryPath, 'subdivisions.json'));
 
-      console.log({ path });
-    })
-  });
 
-console.log({ countries });
+      console.log({ code, subdivisions });
+    }
+  }
+
+  console.log({ countries });
+
+  return true;
+};
+
+build();
+
+// glob('./data/**/*.json')
+//   .then(contents => {
+//     console.log({ contents });
+
+//     contents.forEach((relativePath) => {
+//       const path = relativePath.replace('./data/', '');
+
+//       const foundCountries = countries.filter((country) => {
+//         const { id } = country;
+//         // if ( )
+//       })
+
+//       console.log({ path });
+//     })
+//   });
